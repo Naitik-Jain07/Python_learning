de7b93745cb33a6e4fcb16957109e6045d72805d
Naitik Kankariya <naitikj8200@gmail.com>
2026-05-30 15:56:59 +0530

    Add files via upload

---
+++ day2_project.py
@@ -0,0 +1,13 @@
+print("_______WELCOME TO TIP CALCULATOR______")
+
+Total_price = int (input("whats the total bill: "))
+
+tip = int (input("whats the tip u wanna add :10 12 15"))
+
+
+people = int(input("enter total people:"))
+
+share = (Total_price + tip)/people
+
+print (f" Each person have to pay: {share}")
+
