ca3b6078f80128859a59c7c7a7fae8f0186db64c
Naitik Kankariya <naitikj8200@gmail.com>
2026-06-18 11:43:10 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,17 @@
+question_data = [
+    {"text": "Pikachu is an Electric-type Pokémon.", "answer": "True"},
+    {"text": "Bulbasaur is Pokémon in the National Pokedex. ", "answer": "True"},
+    {"text": "Charizard is a Dragon and Fire type Pokémon.", "answer": "False"},
+    {"text": "Ash Ketchum's first Pokémon was Squirtle.", "answer": "False"},
+    {"text": "Mewtwo was created from Mew's DNA.", "answer": "True"},
+    {"text": "Jigglypuff evolves into Clefairy.", "answer": "False"},
+    {"text": "Gyarados evolves from Magikarp.", "answer": "True"},
+    {"text": "Snorlax is a Fighting-type Pokémon.", "answer": "False"},
+    {"text": "Team Rocket's motto begins with 'Prepare for trouble!'", "answer": "True"},
+    {"text": "Misty is a Water-type Gym Leader.", "answer": "True"},
+    {"text": "Brock specializes in Fire-type Pokémon.", "answer": "False"},
+    {"text": "Ash won the Alola League.", "answer": "True"},
+    {"text": "Lucario was introduced in Generation III.", "answer": "False"},
+    {"text": "Arceus is known as the creator of the Pokémon universe.", "answer": "True"},
+    {"text": "Ditto can breed with almost every breedable Pokémon.", "answer": "True"}
+]
